3b159ef28120fcb05b0b4a7441f24d8c0489eb8f
Vincent Testagrossa <lancevancevt@gmail.com>
2023-11-02 15:22:23 -0400

    copied listing.cc from p1 to p2, ran make, then ran the automated tests

--- src/listing.cc
+++ src/listing.cc
@@ -1,19 +1,34 @@
 // Compiler Theory and Design
-// Dr. Duane J. Jarc
+// Project 1
+// 02NOV2023
+// Vincent Testagrossa
 
 // This file contains the bodies of the functions that produces the compilation
 // listing
 
+// Changed the error string to a vector called errors to store multiple errors per line
+// then loop through and display them all when display is called, clearing the 'queue' after
+// completion.
+
+// Kept the totalErrors int and added ones for syntax, lexical, and semantic errors to keep the
+// count for display in the lastLine function.
+
+// added a check to lastline for 0 total errors, so "Compiled Successfully" can be displayed.
+
 #include <cstdio>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 #include "listing.h"
 
 static int lineNumber;
-static string error = "";
-static int totalErrors = 0;
+static vector<string> errors;
+static int totalErrors = 0,
+			lexicalErrors = 0,
+			syntaxErrors = 0,
+			semanticErrors = 0;
 
 static void displayErrors();
 
@@ -35,6 +50,14 @@ int lastLine()
 	printf("\r");
 	displayErrors();
 	printf("     \n");
+	if (totalErrors > 0){
+		printf("Lexical errors: %4d\n", lexicalErrors);
+		printf("Semantic errors: %4d\n", semanticErrors);
+		printf("Syntactic errors: %4d\n", syntaxErrors);
+	}
+	else {
+		printf("Compiled Successfully\n");
+	}
 	return totalErrors;
 }
     
@@ -43,14 +66,27 @@ void appendError(ErrorCategories errorCategory, string message)
 	string messages[] = { "Lexical Error, Invalid Character ", "",
 		"Semantic Error, ", "Semantic Error, Duplicate Identifier: ",
 		"Semantic Error, Undeclared " };
-
-	error = messages[errorCategory] + message;
+	errors.push_back(messages[errorCategory] + message);
+	switch(errorCategory){
+		case LEXICAL:
+			lexicalErrors++;
+			break;
+		case SYNTAX:
+			syntaxErrors++;
+			break;
+		case GENERAL_SEMANTIC:
+			semanticErrors++;
+			break;
+	}
 	totalErrors++;
 }
 
 void displayErrors()
 {
-	if (error != "")
-		printf("%s\n", error.c_str());
-	error = "";
+	for (const string& error : errors){
+		if (error != ""){
+			printf("%s\n", error.c_str());
+		}
+	}
+	errors.clear();
 }
